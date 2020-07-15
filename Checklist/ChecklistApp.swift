59185eacb61b1ae4d109329a6c514575e7efbf48
mithem <mithem@github.com>
2020-07-15 10:20:29 +0200

    Initial Commit

---
+++ Checklist/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by Miguel Themann on 15.07.20.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
